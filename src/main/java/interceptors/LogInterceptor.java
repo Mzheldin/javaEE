ee1a5d7419b3bde7926b07cd86ae1908e30fcc7c
Mzheldin <mzheldin@yandex.ru>
2019-08-26 07:53:35 +0300

    lesson6
    - in loginterceptor sout changed to logger

--- src/main/java/interceptors/LogInterceptor.java
+++ src/main/java/interceptors/LogInterceptor.java
@@ -1,13 +1,19 @@
 package interceptors;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
 
 public class LogInterceptor {
+
+    private Logger log = LoggerFactory.getLogger("interceptLog");
+
     @AroundInvoke
     public Object printLog(InvocationContext context) throws Exception {
-        System.out.println("Метод: " + context.getMethod().getName());
+        log.info("Метод: " + context.getMethod().getName());
         return context.proceed();
     }
 }
